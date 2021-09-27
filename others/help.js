22cb4150496208f80b5073d0a4c9fce8516a86f8
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-09-27 18:11:07 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,26 +18,26 @@ module.exports = {
     .setThumbnail(`https://images-ext-1.discordapp.net/external/ywW90Qq3ButrY58SXGKsfdaHJ5Fl3fAX-jCt66kxq2k/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807350534901071932/323c09ffbcd4777d8b5d14ac80df56a5.png `)
     .setTitle(`**Panda Help**`)
     .setDescription(`
-User Commands
+**User Commands**
 \`invite\` - \`support\` - \`about\`
 \`ping\` - \`prefix\` - \`uptime\`
 \`tinvites\` - \`sug\` - \`roleuser\`
 \`report\`
 
-Music Commands
+**Music Commands**
 \`play\` - \`skip\` - \`skipto\`
 \`stop\` - \`volume\` - \`nowplaying\`
 \`shuffle\` - \`search\` - \`resume\`
 \`remove\` - \`queue\` - \`loop\`
 \`lyrics\` - \`radio\`
 
-Gif Commands
+**Gif Commands**
 \`boy\` - \`girl\`
 
 
 Links
-[Support](https://discord.gg/progresshome)
-[Invite](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)
+[Support](https://discord.gg/AaC2MCVupt)
+[Invite](https://discord.com/api/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
