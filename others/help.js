4a38dea19dd6f5ac2ba33c2adfc5948bde4b0982
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-09-30 02:59:11 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,25 +18,32 @@ module.exports = {
     .setImage(`https://media.discordapp.net/attachments/859628813230932008/892472179202093096/standard_2.gif`)
     .setTitle(`**Red Help**`)
     .setDescription(`
-**Info Command**
-> invite, support, about
-> ping, prefix, uptime
-> tinvites, sug, roleuser 
 
-**Music Commands**
-> play, skip, skipto
-> stop, volume, nowplaying
-> shuffle, search, resume
-> remove, queue, loop
-> lyrics, radio 
+─── 🔴 ── ✦ ── 🔴 ───
+**🌐┊Info Command** 
 
-**Gif Commands**
-> boy, girl
+invite : support : about
+ping : prefix : uptime
+tinvites : sug : roleuser 
 
+─── 🔴 ── ✦ ── 🔴 ───
+**🎵┊Music Commands** 
 
-Links
-[Support](https://discord.gg/AaC2MCVupt)
-[Invite](https://discord.com/api/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
+play : skip : skipto
+stop : volume{vol} : nowplaying
+shuffle : search : resume
+remove : queue : loop
+lyrics : radio 
+
+─── 🔴 ── ✦ ── 🔴 ───
+**🌝┊Gif Commands** 
+
+boy : gir : anime 
+
+─── 🔴 ── ✦ ── 🔴 ───
+**<:Redbot:892922899571310642>┊Click 
+
+[Support Server](https://discord.gg/AaC2MCVupt) : [Invite Bot](https://discord.com/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("#ff0000");
