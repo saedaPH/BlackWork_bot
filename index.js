7d7af93392184eb216466e8dd312b5c64533c9c6
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-09-30 20:51:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -691,7 +691,34 @@ function delay(delayInms) {
  });
 }
 
-client.on("ready", () => {
-  var join = client.channels.get("887060530857119834'"); /// ئاید ڤۆیس
-  if (join) join.join();
-});
+
+client.on('message', message => {
+
+  if(message.content === `${prefix}lock`) {
+                      if(!message.channel.guild) return message.reply('** This command only for servers :x: **');
+
+if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply(' ** You dont have `MANAGE_CHANNELS` permission **');
+         message.channel.overwritePermissions(message.guild.id, {
+       SEND_MESSAGES: false
+
+         }).then(() => {
+             message.reply("** :white_check_mark: | Chat Closed  **")
+         });
+           }
+
+if(message.content === `${prefix}unlock`) {
+                   if(!message.channel.guild) return message.reply('** This command only for servers :x: **');
+
+if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply('** You dont have `MANAGE_CHANNELS` permission **');
+         message.channel.overwritePermissions(message.guild.id, {
+       SEND_MESSAGES: true
+
+         }).then(() => {
+             message.reply("**:white_check_mark: | Chat Opened **")
+         });
+           }
+           
+    
+  
+}); 
+
