690c505d08ecfa3a43bee671724bee416a05cf04
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-03 09:22:21 +0300

    Update pboy.js

--- Photo command/pboy.js
+++ Photo command/pboy.js
@@ -1,20 +1,42 @@
-const Discord = require( "discord .js") ;
-module.exports . run = async (bot, message, args) =>
-let replies ["  "];
-let result = Math. floor ( (Math. random() * replies . lengt
-let gifembed new Discord. MessageEmbed ()
-setTitle( "pboy")
-setColor ( "#F21313")
-setFooter (" ${message . author. tag} ', message
-. author .avi
-.setImage(replies [result] ) ;
-message.channel. send (gifembed) ;
-exports.conf {
-enabled: true,
-guildOnly: false,
-aliases:['Pboy', "boyP'. 'pboy', "boyp'],
-permLevel: 0
-exports.help {
-name: boy',
-description: 'rexuss",
-usage: 'boy'
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let replies = ["","","","","","","","","","","","","https://cdn.discordapp.com/attachments/699339066029768796/736977867811979324/a_c21ae57fdd3ad6ac99cd038b6a5aa1e4.gif","https://cdn.discordapp.com/attachments/699339066029768796/736148143678291968/15.gif","https://cdn.discordapp.com/attachments/699339066029768796/735864299024810155/20200723_142037.gif","https://cdn.discordapp.com/attachments/699339066029768796/735910966792945764/image0.gif","https://cdn.discordapp.com/attachments/699339066029768796/735910962762350642/image0.gif","https://cdn.discordapp.com/attachments/699339066029768796/735194170230308904/a_0690a3032ce9908e230d7f71dd9a6988_1.gif","https://cdn.discordapp.com/attachments/699339066029768796/734965436663464076/a_4f5d107e5160642113337218f63a6441.gif","https://cdn.discordapp.com/attachments/699339066029768796/734965350357008384/a_8f2bce5f3a3312e7e95236a39ea70efe.gif","https://cdn.discordapp.com/attachments/699339066029768796/734965015739629599/image0-6.gif","https://cdn.discordapp.com/attachments/699339066029768796/732175141567725639/image0.gif","https://cdn.discordapp.com/attachments/699339066029768796/730498782265081876/bab3.gif","https://cdn.discordapp.com/attachments/699339066029768796/730499512602329198/14.gif"];
+let result = Math.floor((Math.random() * replies.length));
+
+let gifembed = new Discord.MessageEmbed()
+
+.setTitle("Baby")
+    
+.setColor("RANDOM")
+
+.setFooter(`${message.author.tag} `, message.author.avatarURL)
+
+.setImage(replies[result]);
+
+message.channel.send(gifembed);
+
+};
+
+exports.conf = {
+
+  enabled: true,
+
+  guildOnly: false,
+
+  aliases: ['gif-baby','baby-gif','gifbaby','babygif'],
+
+  permLevel: 0
+
+};
+
+exports.help = {
+
+  name: 'baby',
+
+  description: 'rexuss',
+
+  usage: 'baby'
+
+};
