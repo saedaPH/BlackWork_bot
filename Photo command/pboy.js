00cb0d83011eb189e7632708e9228c91289b6ba3
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-03 09:20:44 +0300

    Create pboy.js

---
+++ Photo command/pboy.js
@@ -0,0 +1,20 @@
+const Discord = require( "discord .js") ;
+module.exports . run = async (bot, message, args) =>
+let replies ["  "];
+let result = Math. floor ( (Math. random() * replies . lengt
+let gifembed new Discord. MessageEmbed ()
+setTitle( "pboy")
+setColor ( "#F21313")
+setFooter (" ${message . author. tag} ', message
+. author .avi
+.setImage(replies [result] ) ;
+message.channel. send (gifembed) ;
+exports.conf {
+enabled: true,
+guildOnly: false,
+aliases:['Pboy', "boyP'. 'pboy', "boyp'],
+permLevel: 0
+exports.help {
+name: boy',
+description: 'rexuss",
+usage: 'boy'
