cf2bb55219c02bc2eef6998851df369cc537bfdd
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 10:43:31 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,22 +18,46 @@ module.exports = {
     .setThumbnail(`https://images-ext-1.discordapp.net/external/ywW90Qq3ButrY58SXGKsfdaHJ5Fl3fAX-jCt66kxq2k/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807350534901071932/323c09ffbcd4777d8b5d14ac80df56a5.png `)
     .setTitle(`**Panda Help**`)
     .setDescription(`
-User Commands
-\`invite\` - \`support\` - \`about\`
-\`ping\` - \`prefix\` - \`uptime\`
-\`tinvites\` - \`sug\` - \`roleuser\`
-\`report\`
-Music Commands
-\`play\` - \`skip\` - \`skipto\`
-\`stop\` - \`volume\` - \`nowplaying\`
-\`shuffle\` - \`search\` - \`resume\`
-\`remove\` - \`queue\` - \`loop\`
-\`lyrics\` - \`radio\`
-Gif Commands
-\`boy\` - \`girl\`
-Links
-[Support](https://discord.gg/progresshome)
-[Invite](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)
+╭───────╯infooo╰───────╮
+infooo┊Info Command
+invite : support : about : ping : prefix : sug : roleuser 
+╭───────╯modeee╰───────╮
+modeee┊Moderantor
+
+ban : kick : clear
+unban : lock : unlock
+
+╭───────╯musicc╰───────╮
+musicc┊Music Commands
+
+play : skip : skipto
+stop : volume{vol} : nowplaying
+shuffle : search : resume
+remove : queue : loop
+lyrics : radio 
+
+╭───────╯gifff╰───────╮
+gifff┊Gif Commands
+
+boy : girl : anime : couple
+sad : cartoon : neon : rules
+welcome
+
+╭───────╯╰───────╮
+maple_leaf┊Photo Commands
+pboy : pgirl : panime
+pemoji : pwelcome : rules
+
+╭───────╯Redbot╰───────╮
+:Redbot:┊Click
+
+:kurdd:┊Kurd Designer 
+:youuu:┊Youtube KD
+---------------------
+phh┊Progress Home
+youuu┊Youtube PH
+---------------------
+invitee┊Invite Bot
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
