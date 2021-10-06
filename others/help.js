0ec76966643d4c9c223dace2f6b0fc7b8adbff23
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 15:24:10 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,25 +15,29 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setIMAGE(`https://images-ext-2.discordapp.net/external/VFTa1xYCSei5FJyC7JD0yTs-1Qz66SBxOxI2GQx0VWo/https/media.discordapp.net/attachments/859628813230932008/892472179202093096/standard_2.gif`)
-    .setTitle(`**<:Redbot:892922899571310642>Red Bot**`)
+    .setThumbnail(`https://images-ext-1.discordapp.net/external/ywW90Qq3ButrY58SXGKsfdaHJ5Fl3fAX-jCt66kxq2k/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807350534901071932/323c09ffbcd4777d8b5d14ac80df56a5.png `)
+    .setTitle(`**Panda Help**`)
     .setDescription(`
-**<:infooo:895154616046583829>┊𝗜𝗻𝗳𝗼 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
-**invite : support : about : ping : prefix : uptime : invites : sug : roleuser**
-**<:modeee:895155174115516456>┊𝗠𝗼𝗱𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
-**ban : kick : clear : unban : lock : unlock**
-**<:gifff:895160463799779388>┊𝗚𝗶𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
-**boy : girl : anime : couple : sad : cartoon : neon**
-**<:musicc:895157716031520848>┊𝗠𝘂𝘀𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
-**play : skip : stop : vol : search : resume : remove : queue : loop :radio**
-**<:invitee:895164789150593064>┊𝗖𝗹𝗶𝗰𝗸**
-<:kurdd:895161567409221702>┊[Kurd Designer](https://discord.gg/AaC2MCVupt) : <:youuu:895167415774740521>┊[Youtube KD](https://youtube.com/channel/UCgtuqXzhuIqLgm912plcENQ) / <:phh:895161682878423070>┊[Progress Home](https://discord.gg/5MWPYkyWsz) : <:youuu:895167415774740521>┊[Youtube PH](https://youtube.com/channel/UCHYQh0HXyYPO4FvGhwcTNiQ)
-<:invitee:895164789150593064>┊[Invite Bot](https://discord.com/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
-
+User Commands
+\`invite\` - \`support\` - \`about\`
+\`ping\` - \`prefix\` - \`uptime\`
+\`tinvites\` - \`sug\` - \`roleuser\`
+\`report\`
+Music Commands
+\`play\` - \`skip\` - \`skipto\`
+\`stop\` - \`volume\` - \`nowplaying\`
+\`shuffle\` - \`search\` - \`resume\`
+\`remove\` - \`queue\` - \`loop\`
+\`lyrics\` - \`radio\`
+Gif Commands
+\`boy\` - \`girl\`
+Links
+[Support](https://discord.gg/progresshome)
+[Invite](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-   .setColor("#F21313");
-   message.react("")
+   .setColor("RANDOM");
+   message.react("<:emoji_4:822203026776391711>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
