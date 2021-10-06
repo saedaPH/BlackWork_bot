e1ded40846c2fa1763a0b24eaa61e5fa61dd39db
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 11:25:32 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,1 +1,29 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
 
+  
+
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  cooldown: 8,
+  description: "**all commands**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setIMAGE(``)
+    .setTitle(`**Red Bot**`)
+    .setDescription(`
+
+`)
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("#F21313");
+   message.react("")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
