c48a347be5fd6c76220942cff279501d1f5e582d
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 07:47:21 +0300

    Delete clear.js

--- Music/clear.js
+++
@@ -1,9 +0,0 @@
-client.on("message", msg => {
-if(msg.content.startsWith(prefix+"clear")) {
- let args = msg.content.split(" ").slice(1);
- if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("You can't use this command!");
-        if(!args[0]) return msg.channel.send("Specify how many messages you want to delete.");
-        msg.delete();
-        msg.channel.bulkDelete(args[0]).catch(e => { msg.channel.send("You can only delete 100 messages at once.")});
-        msg.channel.send(`Successfully deleted \`${args[0]} messages\``).then(m => m.delete({ timeout: 5000 }));
-}})
