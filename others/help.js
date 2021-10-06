637d68596a0f7659565d6ff2b7c0a92f355be4f1
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 15:29:46 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,19 +18,16 @@ module.exports = {
     .setThumbnail(`https://images-ext-1.discordapp.net/external/ywW90Qq3ButrY58SXGKsfdaHJ5Fl3fAX-jCt66kxq2k/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807350534901071932/323c09ffbcd4777d8b5d14ac80df56a5.png `)
     .setTitle(`**Panda Help**`)
     .setDescription(`
-User Commands
-\`invite\` - \`support\` - \`about\`
-\`ping\` - \`prefix\` - \`uptime\`
-\`tinvites\` - \`sug\` - \`roleuser\`
-\`report\`
-Music Commands
-\`play\` - \`skip\` - \`skipto\`
-\`stop\` - \`volume\` - \`nowplaying\`
-\`shuffle\` - \`search\` - \`resume\`
-\`remove\` - \`queue\` - \`loop\`
-\`lyrics\` - \`radio\`
-Gif Commands
-\`boy\` - \`girl\`
+
+**<:infooo:895154616046583829>┊𝗜𝗻𝗳𝗼 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**invite : support : about : ping : prefix : uptime : invites : sug : roleuser**
+**<:modeee:895155174115516456>┊𝗠𝗼𝗱𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**ban : kick : clear : unban : lock : unlock**
+**<:gifff:895160463799779388>┊𝗚𝗶𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**boy : girl : anime : couple : sad : cartoon : neon**
+**<:musicc:895157716031520848>┊𝗠𝘂𝘀𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**play : skip : stop : vol : search : resume : remove : queue : loop :radio**
+**<:invitee:895164789150593064>┊𝗖𝗹𝗶𝗰𝗸**
 Links
 [Support](https://discord.gg/progresshome)
 [Invite](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)
