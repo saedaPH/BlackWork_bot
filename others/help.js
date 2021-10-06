628cf3ea4b3adec5fd8420b614655fc36fa4737e
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 12:08:25 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,6 +18,17 @@ module.exports = {
     .setIMAGE(``)
     .setTitle(`**Red Bot**`)
     .setDescription(`
+**<:infooo:895154616046583829>┊𝗜𝗻𝗳𝗼 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**invite : support : about : ping : prefix : uptime : invites : sug : roleuser**
+**<:modeee:895155174115516456>┊𝗠𝗼𝗱𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**ban : kick : clear : unban : lock : unlock**
+**<:gifff:895160463799779388>┊𝗚𝗶𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**boy : girl : anime : couple : sad : cartoon : neon**
+**<:musicc:895157716031520848>┊𝗠𝘂𝘀𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**play : skip : stop : vol : search : resume : remove : queue : loop :radio**
+**<:invitee:895164789150593064>┊𝗖𝗹𝗶𝗰𝗸**
+<:kurdd:895161567409221702>┊[Kurd Designer](https://discord.gg/AaC2MCVupt) : <:youuu:895167415774740521>┊[Youtube KD](https://youtube.com/channel/UCgtuqXzhuIqLgm912plcENQ) / <:phh:895161682878423070>┊[Progress Home](https://discord.gg/5MWPYkyWsz) : <:youuu:895167415774740521>┊[Youtube PH](https://youtube.com/channel/UCHYQh0HXyYPO4FvGhwcTNiQ)
+<:invitee:895164789150593064>┊[Invite Bot](https://discord.com/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
 
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
