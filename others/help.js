45aa00679018289117c30703e51ffda687bd43a0
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-06 10:41:19 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,26 +1,44 @@
-const { MessageEmbed } = require("discord.js");
-const { lineReply } = require("discord-reply");
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "Display all commands and descriptions",
+  cooldown: 8,
+  description: "**all commands**",
   execute(message) {
     let commands = message.client.commands.array();
-     
-    let helpEmbed = new MessageEmbed()
-    .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-    .setColor(`#FF0000`)
-    .setTitle("Commands")
-    .addField("General Commands", `\`help\`, \`ping\`, \`prefix\`, \`uptime\`, \`avatar\`, \`status\`, \`suggest\`, \`userinfo\`, \`serverinfo\`, \`botinfo\`, \`github\``)
-    .addField("Music Commands", `\`filter\`, \`loop\`, \`lyrics\`, \`np\`, \`pause\`, \`play\`, \`queue\`, \`radio\`, \`remove\`, \`resume\`, \`search\`, \`shuffle\`, \`skip\`, \`skipto\`, \`stop\`, \`volume\``)
-    .addField("Moderation Commands", `\`kick\`, \`ban\`, \`mute\`, \`unmute\`, \`clear\`, \`lock\`, \`unlock\`, `)
-    .addField("Fun Commands", `\`slap\`, \`hug\`, \`poke\`, \`pat\`, \`cuddle\`, \`feed\`, \`kiss\``)
-    .addField("Gif Commands", `\`boygif\`, \`girlgif\`, \`couplegif\`, \`animegif\`, \`emojigif\`, \`animalgif\``)
-    .addField("Banner Commands", `\`bboy\`, \`bgirl\`, \`banime\``)
-    .addField("Games Commands", `\`rps\`, \`iq\`, \`howgay\`, \`8ball\`, \`rate\`, \`dicksize\``)
-    helpEmbed.setTimestamp();
 
-    message.lineReplyNoMention(helpEmbed).catch(console.error);
+    let helpEmbed = new MessageEmbed()
+    .setThumbnail(`https://images-ext-1.discordapp.net/external/ywW90Qq3ButrY58SXGKsfdaHJ5Fl3fAX-jCt66kxq2k/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807350534901071932/323c09ffbcd4777d8b5d14ac80df56a5.png `)
+    .setTitle(`**Panda Help**`)
+    .setDescription(`
+User Commands
+\`invite\` - \`support\` - \`about\`
+\`ping\` - \`prefix\` - \`uptime\`
+\`tinvites\` - \`sug\` - \`roleuser\`
+\`report\`
+Music Commands
+\`play\` - \`skip\` - \`skipto\`
+\`stop\` - \`volume\` - \`nowplaying\`
+\`shuffle\` - \`search\` - \`resume\`
+\`remove\` - \`queue\` - \`loop\`
+\`lyrics\` - \`radio\`
+Gif Commands
+\`boy\` - \`girl\`
+Links
+[Support](https://discord.gg/progresshome)
+[Invite](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)
+`)
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM");
+   message.react("<:emoji_4:822203026776391711>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
   }
 };
