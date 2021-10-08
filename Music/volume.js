9c5f0e6043c573fb35bfb84a06063e53c5ad9f48
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-08 04:06:34 +0300

    Update volume.js

--- Music/volume.js
+++ Music/volume.js
@@ -43,7 +43,7 @@ execute(message, args) {
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     //define approve embed
     const volinfosetembed = new MessageEmbed()
-    .setColor("RANDOM")
+    .setColor("#F21313")
     .setTitle(`🔊 Volume changed to: \`${args[0]}%\`!`)
     //Send approve message
     return queue.textChannel.send(volinfosetembed).catch(console.error);
