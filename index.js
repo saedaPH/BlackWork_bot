599f54a566f9fa794a12190c85b1d68da5acd0f9
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-13 21:56:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,28 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 /////////////////////////
 
 /////////////////////////
+```js
+client.on("message", message => {
+  if (message.content.startsWith(PREFIX + "girl")) {
+    let girl = [
+"https://media.discordapp.net/attachments/762954136744099842/799811562427777063/Enes_Acar_15.gif", "https://media.discordapp.net/attachments/762954136744099842/800835842422734898/4270_Panda.gif", "https://media.discordapp.net/attachments/699520919328129055/803613686656401418/Haraketli_Emoji_261.gif"
+    ];
 
+    message.channel
+      .send({
+        embed: {
+          description: `${message.author.username} GIRL GIFS `,
+          image: {
+            url: girl[Math.floor(Math.random() * girl.length)]
+          }
+        }
+      })
+      .catch(e => {
+        client.log.error(e);
+      });
+  }
+});
+```
 /////////////////////////
 client.on("message", msg => {
 if(msg.content.startsWith(PREFIX + "clear")) {
