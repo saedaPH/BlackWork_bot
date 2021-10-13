b79d2fcd19c227f5413ac42c6afc04a64accf034
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-13 22:03:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 client.on("message", message => {
   if (message.content.startsWith(PREFIX + "emoji")) {
     let man = [
-
+"https://media.discordapp.net/attachments/762954136744099842/799811562427777063/Enes_Acar_15.gif",
+ "https://media.discordapp.net/attachments/762954136744099842/800835842422734898/4270_Panda.gif", 
+"https://media.discordapp.net/attachments/699520919328129055/803613686656401418/Haraketli_Emoji_261.gif"
     ];
 
     message.channel
