8d1ec6bdbd50c947fe257d15acc1aebbb1513ae5
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-14 06:39:18 +0300

    Update play.js

--- Music/play.js
+++ Music/play.js
@@ -1,1 +1,130 @@
+const { play } = require("../include/play");
+const { Client, Collection, MessageEmbed } = require("discord.js");
+const { attentionembed } = require("../util/attentionembed");
+const { PREFIX } = require(`../config.json`);
+const ytsr = require("youtube-sr")
 
+/////////
+
+module.exports = {
+  name: "play",
+  aliases: ["p"],
+  description: "(p)Plays song from YouTube/Stream",
+  cooldown: 1.5,
+  edesc: `Type this command to play some music.\nUsage: ${PREFIX}play <TITLE | URL>`,
+
+async execute(message, args, client) {
+    //If not in a guild return
+    if (!message.guild) return;
+    //define channel
+    const { channel } = message.member.voice;
+    //get serverqueue
+    const serverQueue = message.client.queue.get(message.guild.id);
+    //If not in a channel return error
+    if (!channel) return attentionembed(message, "Please join a Voice Channel first");
+    //If not in the same channel return error
+    if (serverQueue && channel !== message.guild.me.voice.channel)
+      return attentionembed(message, `You must be in the same Voice Channel as me`);
+    //If no args return
+    if (!args.length)
+      return attentionembed(message, `Usage: ${message.client.prefix}play <YouTube URL | Video Name | Soundcloud URL>`);
+    message.react("<:emoji_4:822203026776391711>").catch(console.error);
+    const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT"))
+      return attentionembed(message, "I need permissions to join your channel!");
+    if (!permissions.has("SPEAK"))
+      return attentionembed(message, "I need permissions to speak in your channel");
+const search = args.join(" ");
+    const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+    const urlValid = videoPattern.test(args[0]);
+    const queueConstruct = {
+      textChannel: message.channel,
+      channel,
+      connection: null,
+      songs: [],
+      loop: false,
+      volume: 69,
+      filters: [],
+      realseek: 0,
+      playing: true
+    };
+
+///////
+
+songInfo = await ytsr.searchOne(search) ;
+        song = {
+          title: songInfo.title,
+          url: songInfo.url,
+          thumbnail: songInfo.thumbnail,
+          duration: songInfo.durationFormatted,
+       };
+      } catch (error) {
+        if (error.statusCode === 403) return attentionembed(message, "Max. uses of api Key, please refresh!");
+        console.error(error);
+        return attentionembed(message, error.message);
+      }
+    }
+    else {
+      try {
+        songInfo = await ytsr.searchOne(search) ;
+        song = {
+          title: songInfo.title,
+          url: songInfo.url,
+          thumbnail: songInfo.thumbnail,
+          duration: songInfo.durationFormatted,
+       };
+      } catch (error) {
+        console.error(error);
+        return attentionembed(message, error);
+      }
+    }
+    let thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png"
+    if (song.thumbnail === undefined) thumb = "https://cdn.discordapp.com/attachments/748095614017077318/769672148524335114/unknown.png";
+    else thumb = song.thumbnail.url;
+    if (serverQueue) {
+      let estimatedtime = Number(0);
+      for (let i = 0; i < serverQueue.songs.length; i++) {
+        let minutes = serverQueue.songs[i].duration.split(":")[0];
+        let seconds = serverQueue.songs[i].duration.split(":")[1];
+        estimatedtime += (Number(minutes)*60+Number(seconds));
+      }
+      if (estimatedtime > 60) {
+        estimatedtime = Math.round(estimatedtime / 60 * 100) / 100;
+        estimatedtime = estimatedtime + " Minutes"
+      }
+      else if (estimatedtime > 60) {
+        estimatedtime = Math.round(estimatedtime / 60 * 100) / 100;
+        estimatedtime = estimatedtime + " Hours"
+      }
+      else {
+        estimatedtime = estimatedtime + " Seconds"
+      }
+serverQueue.songs.push(song);
+      const newsong = new MessageEmbed()
+        .setTitle("🎶 "+song.title)
+        .setURL(song.url)
+        .setColor("RANDOM")
+        .setImage(thumb)
+        .setThumbnail(`https://images-ext-2.discordapp.net/external/sgK9ggHfs-bLZHFzmiOg9V6pw5w0qsW4sN00kU4qMtQ/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807350534901071932/b25a811f2d1306df4c30e34e302bd6c7.png `)
+         .addField("👤 Requested by:", `\`${message.author.username}#${message.author.discriminator}\``, true)
+        .addField("⏱ Length:", `\`${song.duration} Minutes\``, true)
+        .addField("🔊 Volume:", `\`100\``, true)
+        .addField("📈 Position in queue:", `**\`${serverQueue.songs.length - 1}\`**`, true)
+        return serverQueue.textChannel
+        .send(newsong)
+        .catch(console.error);
+
+    }
+//////
+queueConstruct.songs.push(song);
+    message.client.queue.set(message.guild.id, queueConstruct);
+    try {
+      play(queueConstruct.songs[0], message, client);
+    } catch (error) {
+      console.error(error);
+      message.client.queue.delete(message.guild.id);
+      await channel.leave();
+      return attentionembed(message, `Could not join the channel: ${error}`);
+    }
+  }
+};
