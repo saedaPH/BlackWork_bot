35674ceef8154b682b5532ee169ae7a7f68774a1
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-14 07:29:31 +0300

    Create credit.js

---
+++ others/credit.js
@@ -0,0 +1,13 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+module.exports = {
+    name:"credits",
+    run: async(client, message, args) => {
+        let user = message.mentions.users.first() || message.author;
+      
+        let credit = await db.fetch(`credit_${user.id}`)
+        if(credit === null) {credit = 0}
+        return message.channel.send(`${user.username}, have a ${credit} credits`)
+        
+    }   
+}
