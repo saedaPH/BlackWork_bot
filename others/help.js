853c55fd59f807fdfee05ad93de30d3d0d40d926
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-14 21:08:46 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -31,8 +31,7 @@ pboy: pgirl : panime : pneon : panimal : pcouple : psmoking : pbillie
 **<:musicc:898267944109346868>┊𝗠𝘂𝘀𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
 play : skip : stop : vol : search : resume : remove : queue : loop :radio
 **__👤┊𝗖𝗹𝗶𝗰𝗸__**
-<:kurdd:895161567409221702>┊[Kurd Designer](https://discord.gg/AaC2MCVupt) : <:youuu:895167415774740521>┊[Youtube KD](https://youtube.com/channel/UCgtuqXzhuIqLgm912plcENQ)
-[Invite Bot](https://discord.com/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
+[Support Server](https://discord.gg/AaC2MCVupt) : [Invite Bot](https://discord.com/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
 
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
