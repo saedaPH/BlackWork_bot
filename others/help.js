2bb299dcaf657ed354d4d77a7308d8c255ad6144
saedaPH <89024947+saedaPH@users.noreply.github.com>
2021-10-14 10:41:39 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,16 +20,17 @@ module.exports = {
     .setTitle(`**RED BOT**`)
     .setDescription(`
 
-**<:infooo:895154616046583829>┊𝗜𝗻𝗳𝗼 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
-**
+**__<:infooo:895154616046583829>┊𝗜𝗻𝗳𝗼 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀__**
 invite : support : about : ping : prefix : uptime : invites : sug : roleuser
-**<:modeee:895155174115516456>┊𝗠𝗼𝗱𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**__<:modeee:895155174115516456>┊𝗠𝗼𝗱𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀__**
 ban : kick : clear : unban : lock : unlock
-**<:gifff:895160463799779388>┊𝗚𝗶𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**__<:gifff:895160463799779388>┊𝗚𝗶𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀__**
 boy : girl : anime : couple : sad : cartoon : neon : emoji
-**<:musicc:895157716031520848>┊𝗠𝘂𝘀𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀**
+**__<:gifff:895160463799779388>┊ 𝗤𝘂𝗼𝘁𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀__**
+
+**__<:musicc:895157716031520848>┊𝗠𝘂𝘀𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀__**
 play : skip : stop : vol : search : resume : remove : queue : loop :radio
-**<:invitee:895164789150593064>┊𝗖𝗹𝗶𝗰𝗸**
+**__<:invitee:895164789150593064>┊𝗖𝗹𝗶𝗰𝗸__**
 <:kurdd:895161567409221702>┊[Kurd Designer](https://discord.gg/AaC2MCVupt) : <:youuu:895167415774740521>┊[Youtube KD](https://youtube.com/channel/UCgtuqXzhuIqLgm912plcENQ)
 <:invitee:895164789150593064>┊[Invite Bot](https://discord.com/oauth2/authorize?client_id=892061148285894736&permissions=8&scope=bot)
 
